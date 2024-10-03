7127f5b87ab04309ecdc3c0b75374469f1b7c46b
HellStorm Software <65373255+hellstorm-software@users.noreply.github.com>
2024-10-02 23:05:09 -0400

    Update basic-download.bat

--- yt-dl/basic-download.bat
+++ yt-dl/basic-download.bat
@@ -1,2 +1,5 @@
 :: replace "VideoUrlHere" with the url to the playlist or video you want to download. this will download it in the highest quality audio and video as an mp4
 yt-dlp -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]" VideoUrlHere
+
+:: the video's downloaded will need to be converted to h.264 if you are using them in software like premiere
+:: see mp4-conversion.bat for details

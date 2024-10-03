9eeca2649bc165034018a24892f10122e602949f
HellStorm Software <65373255+hellstorm-software@users.noreply.github.com>
2024-10-02 23:09:33 -0400

    Update mp4-conversion.bat

--- yt-dl/mp4-conversion.bat
+++ yt-dl/mp4-conversion.bat
@@ -1,7 +1,9 @@
-@echo off
 ::converts the videos downloaded with basic-dl.bat to h.264 codex for video editing
-cd "C:\videoLocation"
+
+@echo off
 :: replace "videoLocation" with the path to your video
+cd "C:\videoLocation"
+
 ffmpeg -i placeholder.mp4 -c:v libx264 -c:a aac placeholder_n.mp4
 pause
 
